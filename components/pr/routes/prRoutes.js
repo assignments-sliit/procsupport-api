ae10a39906f18dc08de4c408e2b73791a590dbab
Manoj Kumar <dharmakumar338@gmail.com>
2023-05-21 10:05:20 +0530

    PROC-38 PROC-31 PROC-51 Decline and pending status change to PR

--- components/pr/routes/prRoutes.js
+++ components/pr/routes/prRoutes.js
@@ -14,26 +14,28 @@ routes.put(
   "/auth/status/approve",
   prController.checkIfApprover,
   prController.approvePr
-)
+);
 
 routes.put(
   "/auth/status/decline",
   prController.checkIfApprover,
-  prController.declinePr)
+  prController.declinePr
+);
 
 routes.put(
   "/auth/status/pending",
   prController.checkIfApprover,
-  prController.pendingPr)
+  prController.pendingPr
+);
 
-routes.get("/get/all",prController.fetchAllPr)
+routes.get("/get/all", prController.fetchAllPr);
 
-routes.get("/get/auth/all",prController.fetchAllPrWithAuth)
+routes.get("/get/auth/all", prController.fetchAllPrWithAuth);
 
-routes.get("/get/pr/:prid",prController.fetchPrByPrId)
+routes.get("/get/pr/:prid", prController.fetchPrByPrId);
 
-routes.get("/get/auth/pr/:prid",prController.fetchPrByPrIdWithAuth)
+routes.get("/get/auth/pr/:prid", prController.fetchPrByPrIdWithAuth);
 
-routes.get("/get/approved", prController.fetchApprovedPr)
+routes.get("/get/approved", prController.fetchApprovedPr);
 
 module.exports = routes;
